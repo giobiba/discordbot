4a25b2935dfde7aa0fa5a76bab5aa0da934520c0
Rares-Dorian Boza <raresboza@gmail.com>
2023-12-10 16:36:52 +0100

    added precommit hook

--- deploy-commands.js
+++ deploy-commands.js
@@ -25,7 +25,6 @@ for (const folder of commandFolders) {
 
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
-
 (async () => {
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
