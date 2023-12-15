d9a124ef73181d23f317fed184dfcc30a9a385f8
BuildTools <unconfigured@null.spigotmc.org>
2023-12-15 13:17:09 +0200

    play comm fixed with highWaterMark increased and refactoring of join functionality

--- commands/music/join.js
+++ commands/music/join.js
@@ -1,42 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { joinVoiceChannel, VoiceConnectionStatus, entersState } = require('@discordjs/voice');
-
-// Define destroy conection function to free memory
-function handleDisconnect(connection) {
-    return async () => {
-        try {
-            await Promise.race([
-                entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
-                entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
-            ]);
-            // Connection is attempting to reconnect
-        }
-        catch (error) {
-            if (connection && connection.state.status !== VoiceConnectionStatus.Destroyed) {
-                connection.destroy();
-            }
-        }
-    };
-}
+const { joinVC } = require('../../utils/voice_utils.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('join')
         .setDescription('Join the voice channel of the requester.'),
     async execute(interaction) {
-        const channel = interaction.member.voice.channel;
-        if (channel) {
-            const connection = joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guildId,
-                adapterCreator: interaction.guild.voiceAdapterCreator,
-            });
-            // remove listener if it exits
-            connection.off(VoiceConnectionStatus.Disconnected, handleDisconnect(connection));
-            // add listener
-            connection.on(VoiceConnectionStatus.Disconnected, handleDisconnect(connection));
-
-            connection.subscribe(global.player);
+        if (interaction.member.voice.channel) {
+            joinVC(interaction.member.voice.channel);
 
             await interaction.reply({ content: 'Joined!', ephemeral: true });
         }
--- commands/music/play.js
+++ commands/music/play.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { getVoiceConnection, createAudioResource } = require('@discordjs/voice');
+const { joinVC } = require('../../utils/voice_utils.js');
 const ytdl = require('ytdl-core');
 
 module.exports = {
@@ -11,22 +12,29 @@ module.exports = {
             .setDescription('Name of the song')
             .setRequired(false)),
     async execute(interaction) {
-        const connection = getVoiceConnection(interaction.guild.id);
         let url = interaction.options.getString('query');
-
         if (!url) {
             url = 'https://www.youtube.com/watch?v=BokbpfhV8O8';
         }
 
-        if (!connection) {
-            await interaction.reply('Bot is not connected idiot');
-            return;
+        if (!getVoiceConnection(interaction.guild.id)) {
+            if (interaction.member.voice.channel) {
+                joinVC(interaction.member.voice.channel);
+            }
+            else {
+                interaction.reply('You are not connected to a voice channel');
+                return;
+            }
         }
 
-        const stream = ytdl(url, { filter : 'audioonly', volume: 0.5 });
+        const stream = ytdl(url, { filter : 'audioonly', volume: 0.5, highWaterMark: 1 << 25 });
 
-        const resource = createAudioResource(stream);
-        global.player.play(resource);
+        stream.on('progress', (d, total, length) => {
+            console.log('progress', total / length);
+        });
+
+        global.resource = createAudioResource(stream);
+        global.player.play(global.resource);
 
         await interaction.reply('playing....');
     },
