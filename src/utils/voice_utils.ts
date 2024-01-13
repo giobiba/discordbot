6e55cbf643523ef47f6bcc5397e53c8a329f40c8
Giobiba <g.blanar82@gmail.com>
2024-01-13 14:13:07 +0200

    remove handleDisconnect

--- src/utils/voice_utils.ts
+++ src/utils/voice_utils.ts
@@ -1,24 +1,5 @@
 import { VoiceChannel } from 'discord.js';
-import { joinVoiceChannel, VoiceConnectionStatus, entersState, VoiceConnection } from '@discordjs/voice';
-
-// Define the destroy connection function to free memory
-const handleDisconnect = (connection: VoiceConnection): (() => Promise<void>) => {
-    return async (): Promise<void> => {
-        try {
-            await Promise.race([
-                entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
-                entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
-            ]);
-        // Connection is attempting to reconnect
-        }
-        catch (error) {
-            if (connection &&
-                connection.state.status !== VoiceConnectionStatus.Destroyed) {
-                connection.destroy();
-            }
-        }
-    };
-};
+import { joinVoiceChannel } from '@discordjs/voice';
 
 const createVC = (channel: VoiceChannel) => {
     return joinVoiceChannel({
@@ -28,4 +9,4 @@ const createVC = (channel: VoiceChannel) => {
     });
 };
 
-export { handleDisconnect, createVC };
+export { createVC };
